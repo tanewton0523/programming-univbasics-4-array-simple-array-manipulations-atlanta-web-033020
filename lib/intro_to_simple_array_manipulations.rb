8c178c48606124aaebb4c5e6cc04f578fa06f515
TeMecha Newton <tanewton0523@gmail.com>
2020-03-11 23:47:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,6 +19,6 @@ end
 def using_concat(array1, array2)
   array1.concat(array2)
 end 
-def using insert(array, element)
-  array.insert(element)
+def using insert(array, item)
+  array.insert(item)
 end
